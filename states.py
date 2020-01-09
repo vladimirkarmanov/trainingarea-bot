5ee210558f13ced81b817c710d29e248dd1c55c6
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2020-01-10 00:23:53 +0500

    Add states for add_training functionality

--- states.py
+++ states.py
@@ -1,5 +1,11 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
+
 class LevelStates(StatesGroup):
     exercise = State()
     level_num = State()
+
+
+class TrainingStates(StatesGroup):
+    date = State()
+    exercises_repetitions = State()
